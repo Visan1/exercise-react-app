e6229b2a4417b0ee9e220de5d4257bc049679f84
Visan Ly <fruitdealer7@gmail.com>
2023-02-20 23:32:16 -0700

    Added working files

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+
+
+const Footer = () => {
+  return (
+    <Box mt = "80px" bgcolor = "#FFF3F4">
+      <Stack gap = "40px" alignItems = "center" px = "40px" pt = "24px">
+        <Typography variant = "h5" pb = "40px" mt = "20px">
+          Made by VL
+        </Typography>
+      </Stack>
+    </Box>
+  )
+}
+
+export default Footer
